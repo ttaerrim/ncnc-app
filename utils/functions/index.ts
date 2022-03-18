0910f71dda83548b2f388b6ed90d56f137057a79
ttaerrim <ltr0121@naver.com>
2022-03-19 00:02:38 +0900

    [Refactor] 날짜 함수 수정

--- utils/functions/index.ts
+++ utils/functions/index.ts
@@ -5,50 +5,16 @@ export const getSaleRate = (
     return Math.round(((originalPrice - sellingPrice) / originalPrice) * 100);
 };
 
-export const getExpiryDate = (date: string): string => {
-    if (date.length === 24) {
-        const dateArray: string[] = date.substring(0, 10).split('-');
-        return `${dateArray[0]}.${dateArray[1]}.${dateArray[2]}`;
-    } else {
-        const dateArray: string[] = date.split(' ');
-        const month = getMonth(dateArray[1]);
-        const day = dateArray[2];
-        const year = dateArray[3];
-        return `${year}.${month}.${day}`;
-    }
+export const getExpiryDate = (input_date: string): string => {
+    const date = new Date(input_date);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}.${month > 10 ? month : `0${month}`}.${
+        day > 10 ? day : `0${day}`
+    }`;
 };
 
 export const get3DigitsCost = (cost: number): string => {
     return cost.toLocaleString();
-};
-
-const getMonth = (month: string): string => {
-    switch (month) {
-        case 'Jan':
-            return '01';
-        case 'Feb':
-            return '02';
-        case 'Mar':
-            return '03';
-        case 'Apr':
-            return '04';
-        case 'May':
-            return '05';
-        case 'Jun':
-            return '06';
-        case 'Jul':
-            return '07';
-        case 'Aug':
-            return '08';
-        case 'Sep':
-            return '09';
-        case 'Oct':
-            return '10';
-        case 'Nov':
-            return '11';
-        case 'Dec':
-            return '12';
-        default:
-            return '00';
-    }
 };
