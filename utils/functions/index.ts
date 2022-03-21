a7818645460035cc78fffae005e587d890400fb3
ttaerrim <ltr0121@naver.com>
2022-03-22 00:51:43 +0900

    [Fix] getExiryDate 함수 조건 수정

--- utils/functions/index.ts
+++ utils/functions/index.ts
@@ -10,8 +10,8 @@ export const getExpiryDate = (input_date: string): string => {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    return `${year}.${month > 10 ? month : `0${month}`}.${
-        day > 10 ? day : `0${day}`
+    return `${year}.${month >= 10 ? month : `0${month}`}.${
+        day >= 10 ? day : `0${day}`
     }`;
 };
 
